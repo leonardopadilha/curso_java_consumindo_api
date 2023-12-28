e55b3934b19f9318c9817138908cf6a0c1c4b243
leonardopadilha <leonardopadilha28@gmail.com>
2023-12-28 06:15:40 -0300

    Início Nivelamento Técnico - DTAR (Construtores)

--- src/main/java/br/com/dtar/app/App.java
+++ src/main/java/br/com/dtar/app/App.java
@@ -1,7 +1,12 @@
 package br.com.dtar.app;
 
+import br.com.dtar.dto.Viagem;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Teste");
+        Viagem viagem = new Viagem("Londres");
+
+        System.out.println(viagem.destino);
+
     }
 }
--- src/main/java/br/com/dtar/dto/Viagem.java
+++ src/main/java/br/com/dtar/dto/Viagem.java
@@ -1,5 +1,9 @@
 package br.com.dtar.dto;
 
 public class Viagem {
+    public String destino;
 
+    public Viagem(String destino) {
+        this.destino = destino;
+    }
 }
