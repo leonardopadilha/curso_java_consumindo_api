bc3de3617dd2e21ba307e9b69056ad8b7543ad0a
leonardopadilha <leonardopadilha28@gmail.com>
2023-12-31 00:03:55 -0300

    Enum

--- src/main/java/br/com/dtar/app/App.java
+++ src/main/java/br/com/dtar/app/App.java
@@ -1,12 +1,12 @@
 package br.com.dtar.app;
 
 import br.com.dtar.dto.Viagem;
+import br.com.enums.Destinos;
 
 public class App {
     public static void main(String[] args) {
-        Viagem viagem = new Viagem("Londres");
+        Viagem viagem = new Viagem(Destinos.GOIAS);
 
-        System.out.println(viagem.destino);
-
+        System.out.println(viagem.getDestino().getCidade());
     }
 }
--- src/main/java/br/com/dtar/dto/Viagem.java
+++ src/main/java/br/com/dtar/dto/Viagem.java
@@ -1,9 +1,19 @@
 package br.com.dtar.dto;
 
-public class Viagem {
-    public String destino;
+import br.com.enums.Destinos;
 
-    public Viagem(String destino) {
+public class Viagem {
+    private Destinos destino;
+
+    public Viagem(Destinos destino) {
+        this.destino = destino;
+    }
+
+    public Destinos getDestino() {
+        return destino;
+    }
+
+    public void setDestino(Destinos destino) {
         this.destino = destino;
     }
 }
