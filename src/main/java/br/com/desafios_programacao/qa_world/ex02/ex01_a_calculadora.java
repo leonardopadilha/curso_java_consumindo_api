938da6e772fb5d2f417d2dc48a32a83ebc5c1265
leonardopadilha <leonardopadilha28@gmail.com>
2024-01-14 06:26:20 -0300

    Calculadora simples

--- src/main/java/br/com/desafios_programacao/qa_world/ex02/ex01_a_calculadora.java
+++ src/main/java/br/com/desafios_programacao/qa_world/ex02/ex01_a_calculadora.java
@@ -1,2 +1,42 @@
-package br.com.desafios_programacao.qa_world.ex02;public class ex01_a_calculadora {
+package br.com.desafios_programacao.qa_world.ex02;
+
+import java.util.Scanner;
+
+public class ex01_a_calculadora {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro valor: ");
+        int primeiroValor = teclado.nextInt();
+
+        System.out.print("Digite o sinal da operacao desejada: ");
+        String operacao = teclado.next();
+
+        System.out.print("Digite o segundo valor: ");
+        int segundoValor = teclado.nextInt();
+
+        switch (operacao) {
+            case "+" :
+                System.out.println(primeiroValor + segundoValor);
+                break;
+            case "-" :
+                System.out.println(primeiroValor - segundoValor);
+                break;
+            case "*" :
+                System.out.println(primeiroValor * segundoValor);
+                break;
+            case "/" :
+                if (segundoValor > 0) {
+                    System.out.println(primeiroValor / segundoValor);
+                    break;
+                } else {
+                    System.out.println("Nao e possivel dividir por zero");
+                    break;
+                }
+            default:
+                System.out.println("Valores invalidos para essa operacao");
+                break;
+        }
+    }
 }
