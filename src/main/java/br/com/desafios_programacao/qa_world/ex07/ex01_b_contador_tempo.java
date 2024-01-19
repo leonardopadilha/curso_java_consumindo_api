ef04bed96a9ce1c439ff1a9f91c325e4d9691010
leonardopadilha <leonardopadilha28@gmail.com>
2024-01-19 06:44:07 -0300

    Alterando contador de tempo

--- src/main/java/br/com/desafios_programacao/qa_world/ex07/ex01_b_contador_tempo.java
+++ src/main/java/br/com/desafios_programacao/qa_world/ex07/ex01_b_contador_tempo.java
@@ -1,29 +1,37 @@
 package br.com.desafios_programacao.qa_world.ex07;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.Scanner;
 
-public class ex01_a_contador_tempo {
-    public static void main(String[] args) {
+public class ex01_b_contador_tempo {
+    public static void main(String[] args) throws InterruptedException {
 
         Scanner teclado = new Scanner(System.in);
 
-        LocalDateTime localTime = LocalDateTime.now();
+        System.out.println("Escolha a opcao desejada: \n" + "1 - Temporizador \n" + "2 - Regressivo");
+        int numDesejado = teclado.nextInt();
 
-        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-        String formattedDate = localTime.format(timeFormatter);
+        System.out.println("Digite o tempo em segundos: ");
+        int tempoDesejado = teclado.nextInt();
 
-
-/*        System.out.print("Informe o tempo maximo do contador de tempo: ");
-        int tempo = teclado.nextInt();
-
-        for (int i = tempo; i > 0; i--) {
-            System.out.println(formattedDate);
-        }*/
-
-        System.out.println("Final: " + formattedDate);
+        if (numDesejado == 1) {
+            for (int i = 1; i <= tempoDesejado; i++) {
+                System.out.println(i);
+                Thread.sleep(1000);
+            }
+        } else if (numDesejado == 2) {
+            for (int i = tempoDesejado; i >= 1; i--) {
+                System.out.println(i);
+                Thread.sleep(1000);
+            }
+        } else {
+            System.out.println("Numero invalido!!");
+        }
+        System.out.println("Fim!!!!");
     }
 
 }
